49196f3c46167ede2cef11c68c9e2780e6417094
greenwhale00 <goreakay@gmail.com>
2023-11-24 14:42:37 +0900

    231124

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,71 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import NavMenu from './NavMenu';
+
+import { KITCHEN, BATH, DESIGN } from '../data/menu'
 
 const Header = () => {
   return (
     <header className='Header'>
       <div className="inner">
-        <nav>
-          <NavMenu />
-        </nav>
-        <h1>
-          <Link to='/'>
-            <img src={process.env.PUBLIC_URL + "/assets/img/logo.png"} alt="" />
-          </Link>
-        </h1>
+        <div className="gnb">
+
+          <nav>
+            <ul>
+              <li>
+                <a href="/"  >욕실</a>
+                <ul className='smenu'>
+                  {
+                    BATH.map((it, idx) => {
+                      return (
+                        <li key={idx}>
+                          <Link to={it.link} >{it.menu}</Link>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </li>
+              <li>
+                <a href="/">부엌</a>
+                <ul className='smenu'>
+                  {
+                    KITCHEN.map((it, idx) => {
+                      return (
+                        <li key={idx}>
+                          <Link to={it.link}>{it.menu}</Link>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </li>
+              <li>
+                <a href="/">디자인</a>
+                <ul className='smenu'>
+                  {
+                    DESIGN.map((it, idx) => {
+                      return (
+                        <li key={idx}>
+                          <Link to={it.link}>{it.menu}</Link>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </li>
+            </ul>
+          </nav>
+
+
+          <h1>
+            <Link to='/'>
+              <img src={process.env.PUBLIC_URL + "/assets/img/logo.png"} alt="" />
+            </Link>
+          </h1>
+
+        </div>
+
+
       </div>
 
     </header>
---
+++ src/data/Menu.js
@@ -0,0 +1,17 @@
+const KITCHEN = [
+  { id: 1, menu: '수전', link: '/kitchen-faucets' },
+  { id: 2, menu: '씽크볼', link: '/kitchen-sinks' },
+]
+
+const BATH = [
+  { id: 1, menu: '수전', link: '/bathroom-faucets' },
+  { id: 2, menu: '세면기', link: '/lavatories' },
+]
+
+const DESIGN = [
+  { id: 1, menu: '디자인 트렌드', link: '/design-trends' },
+  { id: 2, menu: '콜라보레이션', link: '/collaborations' },
+]
+
+
+export { KITCHEN, BATH, DESIGN }
