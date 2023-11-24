122301aa17e0005503860cb37f2f219c8a880fcf
greenwhale00 <goreakay@gmail.com>
2023-11-24 11:10:30 +0900

    231124

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainVisual from './MainVisual'
+import MainContent from './MainContent'
+
+const Main = () => {
+  return (
+    <Main>
+      <MainVisual />
+      <MainContent />
+    </Main>
+  )
+}
+
+export default Main
